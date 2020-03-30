a66621a4ce70655fbe903758578ef202981e1cd8
palugunak <palugunak@gmail.com>
2020-03-30 13:50:00 +0530

    added commit

---
+++ src/com/jetbraina/Main.java
@@ -0,0 +1,13 @@
+package com.jetbraina;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("started java on in intellij");
+        System.out.println("need to work on the corejava");
+        System.out.println("need to work on leetcode");
+        System.out.println("learn the git ");
+    }
+}
